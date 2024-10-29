645b40b63466712dec85f04d59c3da4f1d8dec01
Balaji <93272649+codingisf@users.noreply.github.com>
2024-10-29 10:56:55 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,46 +1,44 @@
 # app/main.py
 from fastapi import FastAPI 
-from app.routes import auth, course , enroll , progress
-from dotenv import load_dotenv
+from app.routes import auth, course , enroll , progress , user
 from fastapi.middleware.cors import CORSMiddleware # from app.JWT_middleware import AuthenticateRequest
 import os
 import uvicorn 
 
 
 
-load_dotenv()
 
-origins = ["http://localhost:5173/","https://course-frontend-klzqfq035-codingisfs-projects.vercel.app/"]
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://127.0.0.1:8000"
+]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,  # Frontend origin
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (POST, GET, etc.)
     allow_headers=["*"],  # Allow all headers (including Authorization, Content-Type)
-    expose_headers=["Content-Disposition"],
 )
 
-# app.use(
-# cors({
-# origin: “http://localhost:5173/”, // Explicitly specify the allowed origin
-# credentials: true, // Important for cookies, authorization headers with HTTPS
-# methods: [“GET”, “POST”, “PUT”, “DELETE”, “PATCH”, “OPTIONS”],
-# allowedHeaders: [
-# “Origin”,
-# “Content-Type”,
-# “Accept”,
-# “Authorization”,
-# “X-Request-With”,
-# ],
-# })
-# );
+
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Access-Control-Allow-Origin"] = "http://localhost:5173"
+    response.headers["Access-Control-Allow-Credentials"] = "true"
+    response.headers["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS, DELETE"
+    response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type"
+    return response
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(course.router, prefix="/course", tags=["Courses"])
 app.include_router(enroll.router,prefix="/dashboard", tags=["Enroll"])
 app.include_router(progress.router,prefix="/dashboard", tags=["[progress]"])
+app.include_router(user.router, tags=["[user]"])
 
 @app.get("/")
 def root():
